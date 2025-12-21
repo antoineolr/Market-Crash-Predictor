14ed4f8c39a341c9562533b3e85a8629caf3dcef
Antoine Ollivier <antoineollivier@MacBook-Air-de-Antoine.local>
2025-12-21 16:54:09 +0100

    main et des modifs

--- main.py
+++ main.py
@@ -9,39 +9,50 @@ from src.core_model import (create_hmm_model,train_hmm,predict_states,predict_pr
 from src.trading_signals import (generate_signals,generate_signals_hysteresis,apply_smoothing,add_transaction_costs,count_transactions,backtest_threshold)
 from src.backtesting import (calculate_portfolio_value,calculate_metrics,backtest_buy_and_hold,backtest_hmm_strategy,compare_strategies)
 from src.visualization import (graph_hidden_states,graph_crisis_prob,graph_trading_signals,graph_comparison,graph_drawdown,hist_metrics)
+import numpy as np
 
 SP500=load_market_data(ticker_SP500,start_date,end_date)
-VIX=load_vix_data(ticker_VIX,end_date)
+VIX=load_vix_data(start_date,end_date)
 
 data=align_datasets(SP500,VIX)
-log_return=compute_log_returns(data)
+compute_log_returns(data)
 
-data=add_features(data,window_vol,momentum_window,rsi_period,volume_window)
+data=add_features(data)
 
-train_data=data[split_date:]
-test_data=data[:split_date]
+train_data=data[:split_date]
+test_data=data[split_date:]
 
 model=create_hmm_model(n_states,covariance_type,n_iter,random_state)
 model=train_hmm(train_data,model)
 
 states=predict_states(test_data,model)
-probabilities=predict_proba(test_data,model)
 
-optimal_threshold=backtest_threshold(train_data,probabilities,thresholds,tax,initial_capital)
+probabilities_test=predict_proba(test_data,model)
+probabilities_train=predict_proba(train_data,model)
 
-signals=generate_signals(probabilities,optimal_threshold)
+optimal_threshold=backtest_threshold(train_data,probabilities_train,thresholds,tax,initial_capital)
+
+signals=generate_signals(probabilities_test,optimal_threshold)
 signals=apply_smoothing(signals,smoothing_window)
 
-metrics_bh=backtest_buy_and_hold(test_data,initial_capital)
-metrics_hmm=backtest_hmm_strategy(test_data,signals,tax,initial_capital)
+metrics_bh, portfolio_bh=backtest_buy_and_hold(test_data,initial_capital)
+metrics_hmm, portfolio_hmm=backtest_hmm_strategy(test_data,signals,tax,initial_capital)
 
 comparison=compare_strategies(metrics_bh,metrics_hmm)
 print(comparison)
 
+running_max_bh = np.maximum.accumulate(portfolio_bh)
+dd_bh = (portfolio_bh - running_max_bh) / running_max_bh
 
+running_max_hmm = np.maximum.accumulate(portfolio_hmm)
+dd_hmm = (portfolio_hmm - running_max_hmm) / running_max_hmm
 
-graph_hidden_states(data,states)
-graph_crisis_prob(data,probabilities,optimal_threshold)
-graph_trading_signals(data,signals)
-graph_comparison(data,)
-graph_drawdown(data,)
+graph_hidden_states(test_data,states)
+graph_crisis_prob(test_data,probabilities_test,optimal_threshold)
+graph_trading_signals(test_data,signals)
+graph_comparison(test_data,portfolio_bh,portfolio_hmm)
+graph_drawdown(test_data,dd_bh,dd_hmm)
+
+print("Optimal threshold :", optimal_threshold)
+print("Nombres de transactions :", count_transactions(signals))
+
